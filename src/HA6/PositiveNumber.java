77e06dce19cd7365711afe0e3e2e9aff6f950c09
Marius Bronner <marius.bronner@inform-software.com>
2015-11-10 15:20:09 +0100

    moved main to own class

--- src/HA6/PositiveNumber.java
+++ src/HA6/PositiveNumber.java
@@ -1,24 +1,10 @@
+package HA6;
+
 import java.math.BigInteger;
 
 public class PositiveNumber
 {
 	private int value;
-
-	/**
-	 * main-Methode zum Testen
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args)
-	{
-		PositiveNumber zs = new PositiveNumber();
-		zs.setDecimal("144");
-		System.out.println("Binaer: " + zs.getBinary());
-		zs.setHexadecimal("affe");
-		System.out.println("Dezimal: " + zs.getDecimal());
-		zs.setBinary("1000101011");
-		System.out.println("Hexadezimal: " + zs.getHexadecimal());
-	}
 
 	/**
 	 * Konstruktor
